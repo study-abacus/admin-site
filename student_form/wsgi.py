70a4261351e01adfecf99fe031509078fbf92efd
Jatin Katyal <jatin.katyal13@gmail.com>
2018-07-08 23:05:24 +0530

    modified:   student_form/wsgi.py

--- student_form/wsgi.py
+++ student_form/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_quickr.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "student_form.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
