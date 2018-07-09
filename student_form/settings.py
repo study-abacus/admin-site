e86566b7f2419ebcbc58af90afb94ef4077af293
Jatin Katyal <jatin.katyal13@gmail.com>
2018-07-09 23:16:34 +0530

    Logging added

--- student_form/settings.py
+++ student_form/settings.py
@@ -151,3 +151,19 @@ STATICFILES_DIRS = (
 )
 
 LOGIN_URL = '/form/login'
+
+LOGGING = {
+	'version': 1,
+	'disable_existing_loggers': False,
+	'handlers': {
+		'console': {
+			'class': 'logging.StreamHandler',
+		},
+	},
+	'loggers': {
+		'django': {
+			'handlers': ['console'],
+			'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+		},
+	},
+}
