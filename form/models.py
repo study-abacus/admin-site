c25c5328be4a69838611fe07261a57f4cf97a7f3
Jatin Katyal <jatin.katyal13@gmail.com>
2018-10-06 02:17:15 +0530

    achievement string bug fixed

--- form/models.py
+++ form/models.py
@@ -67,7 +67,7 @@ class Achievement(models.Model):
 	remarks = models.CharField(max_length = 500)
 
 	def __str__(self):
-		return "{} - {}".format(student.name, score)
+		return "{} - {}".format(self.student.name, score)
 
 	class Meta:
 		ordering = ('-date',)
